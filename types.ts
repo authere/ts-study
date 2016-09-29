c6a03a6b28dd2703f16e19e98398645987583851
Youngsoo Lee <rulura@gmail.com>
2016-09-30 08:12:05 +0900

    sync

--- types.ts
+++ types.ts
@@ -14,3 +14,15 @@ var bar: Barr = {};
 console.log(`typeof Barr ${typeof Barr}`);
 console.log(`typeof bar ${typeof bar}`);
 //console.log(`typeof Bas ${typeof Bas}`);
+
+var power: any;
+var num: number;
+power = num;
+num = power; //ok!
+
+var num: number;
+var str: string;
+var xx: void;
+num = null;
+str = undefined;
+xx = null;
