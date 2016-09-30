0eae996d1ad605df9dafa683b9c879acd9519373
Youngsoo Lee <rulura@gmail.com>
2016-09-30 12:39:18 +0900

    add enum

---
+++ enum.ts
@@ -0,0 +1,11 @@
+enum numOrder {
+    One = 1,
+    Two,
+    Three
+}
+
+var ord = numOrder.One;
+
+console.log(`ord=${ord}`)
+console.log(`numOrder[1]=${numOrder[1]}`)
+console.log(`numOrder["One"]=${numOrder["One"]}`)
